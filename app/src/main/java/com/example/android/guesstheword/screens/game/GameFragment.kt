46f2ac877a267cf1a6e4999471c4e674a0d27276
Utsav kumar <utsavkc24@gmail.com>
2021-04-10 21:10:39 +0530

    viewmodel is created

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -17,11 +17,14 @@
 package com.example.android.guesstheword.screens.game
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.example.android.guesstheword.R
 import com.example.android.guesstheword.databinding.GameFragmentBinding
@@ -30,7 +33,7 @@ import com.example.android.guesstheword.databinding.GameFragmentBinding
  * Fragment where the game is played
  */
 class GameFragment : Fragment() {
-
+    private lateinit var gameViewModel: GameViewModel
     // The current word
     private var word = ""
 
@@ -52,7 +55,8 @@ class GameFragment : Fragment() {
                 container,
                 false
         )
-
+        Log.i("GameFragment", "Called ViewModelProviders.of")
+        gameViewModel = ViewModelProviders.of(this).get(GameViewModel::class.java)
         resetList()
         nextWord()
 
