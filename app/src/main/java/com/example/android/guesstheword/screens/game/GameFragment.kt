e85672eeb09d69e00403fbf163584da06affe6bc
Utsav kumar <utsavkc24@gmail.com>
2021-04-11 20:45:03 +0530

    Timer is added

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -17,10 +17,12 @@
 package com.example.android.guesstheword.screens.game
 
 import android.os.Bundle
+import android.text.format.DateUtils
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -55,6 +57,14 @@ class GameFragment : Fragment() {
         gameViewModel.word.observe(viewLifecycleOwner, Observer { newWord ->
             binding.wordText.text = newWord
         })
+        gameViewModel.hasGameFinished.observe(viewLifecycleOwner, Observer { hasFinished ->
+            if (hasFinished){
+                gameFinished()
+                gameViewModel.onGameFinishCompleted()
+            }
+        })
+        gameViewModel.currentTime.observe(viewLifecycleOwner, Observer { newTime ->
+        binding.timerText.text = DateUtils.formatElapsedTime(newTime)})
         binding.correctButton.setOnClickListener {
             gameViewModel.onCorrect()
         }
--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,12 +1,22 @@
 package com.example.android.guesstheword.screens.game
 
+import android.os.CountDownTimer
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
-
+    companion object {
+        // These represent different important times
+        // This is when the game is over
+        const val DONE = 0L
+        // This is the number of milliseconds in a second
+        const val ONE_SECOND = 1000L
+        // This is the total time of the game
+        const val COUNTDOWN_TIME = 10000L
+    }
+    private val timer : CountDownTimer
     // The current word
     var _word = MutableLiveData<String>()
     val word : LiveData<String>
@@ -17,7 +27,13 @@ class GameViewModel : ViewModel() {
     val score : LiveData<Int>
     get() = _score
 
+    private var _hasGameFinished = MutableLiveData<Boolean>()
+    val hasGameFinished : LiveData<Boolean>
+    get() = _hasGameFinished
 
+    private val _currentTime = MutableLiveData<Long>()
+    val currentTime: LiveData<Long>
+        get() = _currentTime
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
 
@@ -27,11 +43,24 @@ class GameViewModel : ViewModel() {
         nextWord()
         _score.value = 0
         _word.value = ""
+        timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND){
+            override fun onTick(millisUntilFinished: Long) {
+                _currentTime.value = (millisUntilFinished / ONE_SECOND)
+            }
+
+            override fun onFinish() {
+                _currentTime.value = DONE
+                _hasGameFinished.value = true
+            }
+
+        }
+        timer.start()
     }
 
     override fun onCleared() {
         super.onCleared()
         Log.i("GameViewModel", "GameViewModel is destroyed")
+        timer.cancel()
     }
 
     /**
@@ -70,10 +99,10 @@ class GameViewModel : ViewModel() {
     private fun nextWord() {
         //Select and remove a word from the list
         if (wordList.isEmpty()) {
-            //gameFinished()
-        } else {
-            _word.value = wordList.removeAt(0)
+           resetList()
         }
+            _word.value = wordList.removeAt(0)
+
     }
 
     /** Methods for buttons presses **/
@@ -86,6 +115,9 @@ class GameViewModel : ViewModel() {
     fun onCorrect() {
         _score.value = (score.value)?.plus(1)
         nextWord()
+    }
+    fun onGameFinishCompleted(){
+        _hasGameFinished.value = false
     }
 
 }
